dd18200074962ae5fb4101636b6ce46a126f3c03
Ikayomi <alayodavid28@gmail.com>
2026-05-30 00:17:27 +0100

    Added Services component

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,18 @@
-import { Route, Routes } from "react-router-dom";
+
 import Navbar from "./components/Navbar";
 import Exclusive from "./components/Exclusive";
-import Services from "./components/Services";
+import Services from "./components/Services/Services";
 import Traveller from "./components/Travellers";
 import About from "./components/About";
 
 function App() {
   return (
     <>
-      <Routes>
-        <Route path="/" element={<Navbar />} />
-        <Route path="/" element={<Exclusive />} />
-        <Route path="/" element={<Services />} />
-        <Route path="/" element={<Traveller />} />
-        <Route path="/" element={<About />} />
-      </Routes>
+        <Navbar />
+      <Exclusive />
+      <Services />
+      <Traveller />
+      <About />
     </>
   );
 }
--- src/pages/Landingpage.tsx
+++ src/pages/Landingpage.tsx
@@ -1,16 +1,8 @@
-import Navbar from "../components/Navbar";
-import Exclusive from "../components/Exclusive";
-import Services from "../components/Services";
-import Travellers from "../components/Travellers";
-import About from "../components/About";
+
 const Landingpage = () => {
   return (
     <div>
-      <Navbar />
-      <Exclusive />
-      <Services />
-      <Travellers />
-      <About />
+ 
     </div>
   );
 };
