90512838a02436e846d9bc6b10613691d57a9702
J-techk <chrischinedu186@gmail.com>
2026-05-30 05:11:11 +0100

    Resort-project

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,11 @@
 import { Route, Routes } from "react-router-dom";
-import Navbar from "./components/Navbar";
-import Exclusive from "./components/Exclusive";
-import Services from "./components/Services";
-import Traveller from "./components/Travellers";
-import About from "./components/About";
+import Login from "./components/Login";
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Navbar />} />
-        <Route path="/" element={<Exclusive />} />
-        <Route path="/" element={<Services />} />
-        <Route path="/" element={<Traveller />} />
-        <Route path="/" element={<About />} />
+        <Route path="/" element={<Login />} />
       </Routes>
     </>
   );
