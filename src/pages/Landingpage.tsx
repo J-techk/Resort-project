a0a135966d2d19825a6d49b5e7e3e0827c843f8f
J-techk <chrischinedu186@gmail.com>
2026-06-09 14:15:40 +0100

    Add Exclusive component to Landingpage

--- src/pages/Landingpage.tsx
+++ src/pages/Landingpage.tsx
@@ -2,12 +2,13 @@ import About from "../components/About/About";
 import Navbar from "../components/Navbar";
 import Services from "../components/Services/Services"
 import Travellers from "../components/Travellers";
-
+import Exclusive from "../components/Exclusive";
 
 const Landingpage = () => {
   return (
     <div>
       <Navbar/>
+      <Exclusive/>
       <Services />
       <Travellers />
       <About />
